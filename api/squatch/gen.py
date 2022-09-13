3727742c7b869083c9a0964b0db203f9a56b87f4
Dean Pierce <pierce403@gmail.com>
2022-09-13 00:51:22 -0700

    more details

--- api/squatch/gen.py
+++ api/squatch/gen.py
@@ -4,7 +4,7 @@ data={}
 
 for i in range(1,15):
   data['id']=i
-  data['name']="Squatch "+str(i)
+  data['name']="BSidesPDX Squatch #"+str(i)
   data['image']="https://squatch.fun/api/squatch/squatch"+str(i)+".png"
   f=open(str(i)+'.json','w')
   f.write(json.dumps(data))
